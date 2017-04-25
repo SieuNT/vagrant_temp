64453de3e52abf5283141d3395b3e045086ad500
Siêu <SieuNT@users.noreply.github.com>
2017-04-25 15:02:19 +0700

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ DBHOST=localhost
 DBNAME=cms
 DBUSER=root
 DBPASSWD=root
+GITHUB_TOKEN=d50b93a6701a93cc9f869866302eed17e0f8d114
 
 echo -e "\n--- Install MySQL specific packages and settings ---\n"
 debconf-set-selections <<< "mysql-server mysql-server/root_password password $DBPASSWD"
@@ -15,8 +16,10 @@ echo -e "\n--- Setting up our MySQL user and db ---\n"
 mysql -u$DBUSER -p$DBPASSWD -e "CREATE DATABASE $DBNAME"
 mysql -u$DBUSER -p$DBPASSWD -e "grant all privileges on $DBNAME.* to '$DBUSER'@'%' identified by '$DBPASSWD'"
 
+sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
+
 echo -e "\n--- Installing PHP-specific packages ---\n"
-apt-get -y install php apache2 libapache2-mod-php php-curl php-gd php-mysql 
+apt-get -y install php apache2 libapache2-mod-php php-curl php-gd php-mysql php-mbstring php-xml
 
 echo -e "\n--- Enabling mod-rewrite ---\n"
 a2enmod rewrite
@@ -30,3 +33,7 @@ service mysql restart
 echo -e "\n--- Installing Composer for PHP package management ---\n"
 curl --silent https://getcomposer.org/installer | php >> /vagrant/vm_build.log 2>&1
 mv composer.phar /usr/local/bin/composer
+
+composer config -g github-oauth.github.com $GITHUB_TOKEN
+
+apt-get install unzip
