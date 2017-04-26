c89827e15217557d2a6371d25c5e63b126541019
Siêu <SieuNT@users.noreply.github.com>
2017-04-26 15:25:02 +0700

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -5,21 +5,22 @@ DBHOST=localhost
 DBNAME=cms
 DBUSER=root
 DBPASSWD=root
-GITHUB_TOKEN=d50b93a6701a93cc9f869866302eed17e0f8d114
+GITHUB_TOKEN=381936937f91ec41f6d0bc1bd1215a340017aa7e
 
 echo -e "\n--- Install MySQL specific packages and settings ---\n"
 debconf-set-selections <<< "mysql-server mysql-server/root_password password $DBPASSWD"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $DBPASSWD"
 apt-get -y install mysql-server
+apt-get install unzip
 
 echo -e "\n--- Setting up our MySQL user and db ---\n"
-mysql -u$DBUSER -p$DBPASSWD -e "CREATE DATABASE $DBNAME"
+mysql -u$DBUSER -p$DBPASSWD -e "CREATE DATABASE $DBNAME CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"
 mysql -u$DBUSER -p$DBPASSWD -e "grant all privileges on $DBNAME.* to '$DBUSER'@'%' identified by '$DBPASSWD'"
 
 sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
 
 echo -e "\n--- Installing PHP-specific packages ---\n"
-apt-get -y install php apache2 libapache2-mod-php php-curl php-gd php-mysql php-mbstring php-xml
+apt-get -y install php apache2 libapache2-mod-php php-curl php-gd php-mysql php-mbstring php-xml php-intl php-sqlite3 php-pgsql php-memcache memcached php-opcache php-apcu
 
 echo -e "\n--- Enabling mod-rewrite ---\n"
 a2enmod rewrite
@@ -36,4 +37,4 @@ mv composer.phar /usr/local/bin/composer
 
 composer config -g github-oauth.github.com $GITHUB_TOKEN
 
-apt-get install unzip
+composer global require fxp/composer-asset-plugin
